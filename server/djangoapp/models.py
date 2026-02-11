dc3377928048d8d7d82a5100779b246b4792cc98
YusefZahran <yusef.a.zahran@gmail.com>
2026-02-11 17:56:37 +0200

    lint fixes 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -45,7 +45,7 @@ class CarModel(models.Model):
                                 MaxValueValidator(2023),
                                 MinValueValidator(2015)
                                ])
-    
+
     # Other fields as needed
     def __str__(self):
         return self.name  # Return the name as the string representation
