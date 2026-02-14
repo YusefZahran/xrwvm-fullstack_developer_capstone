1745cac72cf5202a80d3baeba07e3cc9420d3c21
YusefZahran <yusef.a.zahran@gmail.com>
2026-02-14 15:43:47 -0500

    last lab

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,8 +94,7 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake\
-                    ": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -148,7 +147,6 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as e:
             print(f'Error; {e}')
-            return JsonResponse({"status": 401, "message": "\
-            Error in posting review"})
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://yusefazahran-8000.theianext-0\
--labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = ['https://yusefazahran-8000.theianext-0\
--labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = ['localhost', 'https://yusefazahran-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = ['https://yusefazahran-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -96,8 +94,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.\
-        UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
